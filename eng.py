52e4af1768e65bba43e8a64d29d99021f1e4f8fe
Yusuf <52000549+koprendo@users.noreply.github.com>
2022-07-28 22:04:07 +0300

    Create eng.py

---
+++ eng.py
@@ -0,0 +1,35 @@
+from functions import *
+from constants import *
+from add import *
+from see import *
+from delete import *
+from example import *
+from count import *
+
+
+def main():
+    while True:
+        name = produce_random_calling()
+        waiting("--> What's your command, " + name + "? \
+(To see commands, type info)", F0)
+        # always smile.
+        command = input(">").strip(ignore_these).lower()
+
+        if command == "add":
+            add()
+        elif command == "see":
+            see()
+        elif command == "exadd":
+            example_add()
+        elif command == "info":
+            os.system("info.txt")
+        else:
+            bye = produce_random_farewell()
+            waiting("\n" + bye + "\n", F2)
+            timer(0.5)
+            break
+
+if __name__ == "__main__":
+    waiting("*" * 5 + "WELCOME**TO**INTERFACE**FOR**ENGLISH**STUDY" + "*" * 5,
+            F0)
+    main()
